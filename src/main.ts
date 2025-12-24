e7e73de29293f0b8c7be9fef40cdffc3417b506d
haewonwon <khcw1029@daum.net>
2025-12-24 09:18:22 +0900

    Chore: 초기 구조 설정

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,23 @@
 import './style.css'
 
-const app = document.getElementById('app')
+class App {
+  private $target: HTMLElement;
 
-app!.innerHTML = '<h1>Hello World</h1>'
+  constructor($target: HTMLElement) {
+    this.$target = $target;
+    this.render();
+  }
+
+  render() {
+    this.$target.innerHTML = `
+      <h1>Mandalart Start</h1>
+    `;
+  }
+}
+
+// index.html의 <div id="app">을 찾아 앱을 시작
+const appElement = document.querySelector<HTMLElement>('#app');
+
+if (appElement) {
+  new App(appElement);
+}
