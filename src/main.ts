94fc37c858186695204f839194818b16b26b6942
haewonwon <khcw1029@daum.net>
2025-12-30 16:14:25 +0900

    Chore: 폴더 구조 잡기

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,20 @@
-import './style.css'
+import './style.css';
+import App from './App';
 
-class App {
-  private $target: HTMLElement;
+// class App {
+//   private $target: HTMLElement;
 
-  constructor($target: HTMLElement) {
-    this.$target = $target;
-    this.render();
-  }
+//   constructor($target: HTMLElement) {
+//     this.$target = $target;
+//     this.render();
+//   }
 
-  render() {
-    this.$target.innerHTML = `
-      <h1>Mandalart Start</h1>
-    `;
-  }
-}
+//   render() {
+//     this.$target.innerHTML = `
+//       <h1>Mandalart Start</h1>
+//     `;
+//   }
+// }
 
 // index.html의 <div id="app">을 찾아 앱을 시작
 const appElement = document.querySelector<HTMLElement>('#app');
